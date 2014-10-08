c0724e68f8e8ae5f4154b125a5b72bdb84442eb8
Boyan Nedyalkov <wintersun1993@abv.bg>
2014-10-08 21:48:03 +0300

    Added Encapsulation-Polymorph-HW

---
+++ Encapsulation-Polymorph-HW/Shapes/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Triangle : BasicShape
+    {
+        public Triangle(double width,double height)
+            : base(width, height)
+        {
+
+        }
+
+        public override double CalculateArea()
+        {
+            return (Width * Height)/2;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Width + Height + Math.Sqrt((Width * Width) + (Height * Height));
+        }
+    }
+}
