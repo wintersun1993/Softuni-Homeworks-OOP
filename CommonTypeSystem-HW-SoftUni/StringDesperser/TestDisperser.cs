6ac388777a7fcf642bcfeaf1cbd240b9227c0cec
Boyan Nedyalkov <wintersun1993@abv.bg>
2014-10-13 21:36:35 +0300

    Added CommonTypeSystem-HW-SoftUni.

---
+++ CommonTypeSystem-HW-SoftUni/StringDesperser/TestDisperser.cs
@@ -0,0 +1,25 @@
+﻿/*
+    Define a class StringDisperser. 
+    •The constructor should take several strings as arguments. 
+    •Implement the ICloneable interface. The Clone() method should make a deep copy of all object fields
+     into a new object of type StringDisperser.
+    •Implement the IEnumerable interface to allow foreach on objects of type StringDisperser.
+     The items returned should be the characters of each string
+*/
+
+using System;
+
+namespace StringDesperser
+{
+    class TestDisperser
+    {
+        static void Main()
+        {
+            StringDisperser stringDisperser = new StringDisperser("gosho", "pesho", "tanio");
+            foreach (var ch in stringDisperser)
+            {
+                Console.Write(ch + " ");
+            }
+        }
+    }
+}
